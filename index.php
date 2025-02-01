6aca7dfc7fd76fd8cf07e9c363380e5626f22a1c
mosa-adekunle <mosa.adekunle@gmail.com>
2025-01-31 22:02:06 -0700

    Updating index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php include "index-controller.php"; ?>
 <html>
 <head>
-    <link href="resources/bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="resources/bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link href="resources/style.css" rel="stylesheet">
-    <script src="resources/bootstrap-5.3.3-dist/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="resources/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
